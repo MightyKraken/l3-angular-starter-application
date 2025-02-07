bc91ffe197ea05bede32077c1b8ef8fdf14429cc
Sabik Abtahee <sabik.abtahee@gmail.com>
2025-02-07 07:02:08 +0600

    prettier run

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,15 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed } from '@angular/core/testing';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			imports: [AppComponent]
 		}).compileComponents();
 	});
 
-	it("should create the app", () => {
+	it('should create the app', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.componentInstance;
 		expect(app).toBeTruthy();
