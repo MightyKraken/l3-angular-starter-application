94a873bdd19ebcfcf6c5c1e583277923ec364903
Sabik Abtahee <sabik.abtahee@gmail.com>
2025-02-17 07:39:21 +0600

    fix: editor config prefix setup

---
+++ src/app/app-root/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+
+import { AppHomeComponent } from '../app-home/app-home.component';
+import { AppPageNotFoundComponent } from '../app-page-not-found/app-page-not-found.component';
+
+export const routes: Routes = [
+	{
+		path: '',
+		component: AppHomeComponent
+	},
+	{ path: '**', component: AppPageNotFoundComponent }
+];
