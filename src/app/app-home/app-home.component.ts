4e7134f9290b0271c19752f3b0bfd22cc44822d7
Sabik Abtahee <sabik.abtahee@gmail.com>
2025-02-17 07:39:21 +0600

    fix: editor config prefix setup

---
+++ src/app/app-home/app-home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-home',
+	imports: [],
+	templateUrl: './app-home.component.html',
+	styleUrl: './app-home.component.scss'
+})
+export class AppHomeComponent {}
