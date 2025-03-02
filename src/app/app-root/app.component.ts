34ad893a9ea22849355dbcf0bcc7ac3ba0239fa7
Sabik Abtahee <sabik.abtahee@gmail.com>
2025-03-02 23:13:12 +0600

    chore: clean up

--- src/app/app-root/app.component.ts
+++ src/app/app-root/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { SHARED_COMPONENTS } from '../app-shared/constants/imports.const';
+
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
-	imports: [RouterOutlet]
+	imports: [RouterOutlet, ...SHARED_COMPONENTS]
 })
 export class AppComponent {}
--- src/app/app-shared/constants/imports.const.ts
+++ src/app/app-shared/constants/imports.const.ts
@@ -1,1 +1,5 @@
+import { AppPageNotFoundComponent } from '../../app-page-not-found/app-page-not-found.component';
+
 export const PRIME_UI_IMPORTS = [];
+
+export const SHARED_COMPONENTS = [AppPageNotFoundComponent];
