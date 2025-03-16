a2ba61a37a5fdc5f723217f283ba9c56bad5eeeb
SabikAbtahee <sabikchamp@gmail.com>
2025-03-16 07:41:24 +0600

    chore: clean up

--- src/app/app-home/app-home.component.ts
+++ src/app/app-home/app-home.component.ts
@@ -1,9 +1,7 @@
-import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-home',
-	imports: [NgClass],
 	templateUrl: './app-home.component.html',
 	styleUrl: './app-home.component.scss'
 })
