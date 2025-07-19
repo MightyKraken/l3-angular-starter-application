fd5d1eb0730b60cbe513c1033da276cb3c43b1ea
SabikAbtahee <sabikchamp@gmail.com>
2025-07-19 13:23:01 +0600

    chore: some changes

--- src/app/app-home/app-home.component.ts
+++ src/app/app-home/app-home.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+
+import { AppHomeService } from './app-home.service';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './app-home.component.html',
-	styleUrl: './app-home.component.scss'
+	styleUrl: './app-home.component.scss',
+	imports: [JsonPipe]
 })
-export class AppHomeComponent {}
+export class AppHomeComponent {
+	appHomeService = inject(AppHomeService);
+	data: unknown = null;
+
+	ngOnInit(): void {
+		// this.appHomeService.getData('').subscribe((data) => {
+		// 	this.data = data;
+		// });
+	}
+}
--- src/app/app-shared/index.ts
+++ src/app/app-shared/index.ts
@@ -1,1 +1,2 @@
+// @index(['./**/*.ts', '!./**/*.spec.ts'], f => `export * from '${f.path}';`)
 export * from './services/app-title-strategy/app-title-strategy.service';
