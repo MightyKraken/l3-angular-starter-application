f3e3e6b53bc18afdcd01d1e27734277d23975a68
SabikAbtahee <sabikchamp@gmail.com>
2025-10-08 19:28:35 +0600

    chore: so many dumb issues

--- src/app/authentication/sign-in/sign-in.component.ts
+++ src/app/authentication/sign-in/sign-in.component.ts
@@ -1,21 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'sign-in',
 	standalone: true,
-	imports: [CommonModule, FormsModule, RouterLink, RouterModule],
+	imports: [CommonModule],
 	templateUrl: './sign-in.component.html',
 	styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent {
 	username: string = '';
 	password: string = '';
-
-	onSubmit(): void {
-		// Handle login logic here
-		console.log('Login attempt with:', { username: this.username });
-	}
 }
