d34e40905545b4e3241a755d2404cba199d26480
SabikAbtahee <sabikchamp@gmail.com>
2025-11-19 20:46:00 +0600

    chore: try out zoneless

--- src/app/app-root/app.config.ts
+++ src/app/app-root/app.config.ts
@@ -1,19 +1,22 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+	ApplicationConfig,
+	provideZonelessChangeDetection
+} from '@angular/core';
 import {
 	provideRouter,
 	TitleStrategy,
 	withComponentInputBinding
 } from '@angular/router';
-import { AppTitleStrategyService } from 'src/app/shared';
 
+import { AppTitleStrategyService } from '../shared';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes, withComponentInputBinding()),
 		provideHttpClient(),
+		provideZonelessChangeDetection(),
 		{ provide: TitleStrategy, useClass: AppTitleStrategyService }
 	]
 };
