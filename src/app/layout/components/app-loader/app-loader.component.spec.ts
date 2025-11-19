350970f36d18a2132074318b1cc1ec25951d8eba
SabikAbtahee <sabikchamp@gmail.com>
2025-11-19 20:09:18 +0600

    chore: karma tests brought back

--- src/app/layout/components/app-loader/app-loader.component.spec.ts
+++ src/app/layout/components/app-loader/app-loader.component.spec.ts
@@ -14,8 +14,8 @@ describe('AppLoaderComponent', () => {
 				{
 					provide: Renderer2,
 					useValue: {
-						addClass: jest.fn(),
-						removeClass: jest.fn()
+						addClass: jasmine.createSpy('addClass'),
+						removeClass: jasmine.createSpy('removeClass')
 					}
 				},
 				DestroyRef
