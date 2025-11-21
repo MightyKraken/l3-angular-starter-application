90ab1ed1af8787153683332b2f1fa871fc8b98fd
SabikAbtahee <sabikchamp@gmail.com>
2025-11-21 07:37:09 +0600

    chore: 404 page made

--- src/app/app-page-not-found/app-page-not-found.component.ts
+++ src/app/app-page-not-found/app-page-not-found.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
 	selector: 'app-page-not-found',
-	imports: [],
+	imports: [RouterLink],
 	templateUrl: './app-page-not-found.component.html',
 	styleUrl: './app-page-not-found.component.scss'
 })
-export class AppPageNotFoundComponent {}
+export class AppPageNotFoundComponent {
+	constructor(private location: Location) {}
+
+	goBack(): void {
+		this.location.back();
+	}
+}
