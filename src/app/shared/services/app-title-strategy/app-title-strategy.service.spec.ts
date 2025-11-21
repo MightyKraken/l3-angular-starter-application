4321933372a0b8a7f72191b82b6bece31854094c
SabikAbtahee <sabikchamp@gmail.com>
2025-11-21 22:13:47 +0600

    chore: vi removed

--- src/app/shared/services/app-title-strategy/app-title-strategy.service.spec.ts
+++ src/app/shared/services/app-title-strategy/app-title-strategy.service.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { Title } from '@angular/platform-browser';
 import { RouterStateSnapshot } from '@angular/router';
 import { environment } from '@environment';
-import { vi } from 'vitest';
 
 import { AppTitleStrategyService } from './app-title-strategy.service';
 
