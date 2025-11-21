d488ff4de395d25c022204ccc68cf0de0ff3a698
SabikAbtahee <sabikchamp@gmail.com>
2025-11-21 19:42:01 +0600

    chore: refactor

--- src/app/app-page-not-found/app-page-not-found.component.ts
+++ src/app/app-page-not-found/app-page-not-found.component.ts
@@ -1,6 +1,6 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { Location } from '@angular/common';
 
 @Component({
 	selector: 'app-page-not-found',
