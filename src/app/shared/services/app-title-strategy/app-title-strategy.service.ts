370d055d527a53adb0cfac87a7844db37bb45c1a
SabikAbtahee <sabik.abtahee@iqvia.com>
2025-11-29 21:00:05 +0600

    chore: modernize code

--- src/app/shared/services/app-title-strategy/app-title-strategy.service.ts
+++ src/app/shared/services/app-title-strategy/app-title-strategy.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
 import { environment } from '@environment';
@@ -7,9 +7,7 @@ import { environment } from '@environment';
 	providedIn: 'root'
 })
 export class AppTitleStrategyService extends TitleStrategy {
-	constructor(private readonly title: Title) {
-		super();
-	}
+	private readonly title = inject(Title);
 
 	override updateTitle(routerState: RouterStateSnapshot): void {
 		const title = this.buildTitle(routerState);
