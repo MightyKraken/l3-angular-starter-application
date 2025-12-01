18dc3db89065a435fb41a51322de99827bcbd69c
SabikAbtahee <sabik.abtahee@iqvia.com>
2025-12-01 18:33:18 +0600

    chore: cleanup project

--- src/app/app-home/app-home.component.ts
+++ src/app/app-home/app-home.component.ts
@@ -1,7 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { COMMON_IMPORTS, SHARED_COMPONENTS } from '@shared';
-
-import { AppLoaderService } from '../layout';
 
 @Component({
 	selector: 'app-home',
@@ -9,21 +7,4 @@ import { AppLoaderService } from '../layout';
 	templateUrl: './app-home.component.html',
 	styleUrl: './app-home.component.scss'
 })
-export class AppHomeComponent implements OnInit {
-	private xa = true;
-	appLoaderService = inject(AppLoaderService);
-	data: unknown = null;
-	x = false;
-	d = 22;
-
-	ngOnInit(): void {
-		this.startLoading();
-	}
-
-	startLoading(): void {
-		this.appLoaderService.show();
-		setTimeout(() => {
-			this.appLoaderService.hide();
-		}, 200);
-	}
-}
+export class AppHomeComponent {}
