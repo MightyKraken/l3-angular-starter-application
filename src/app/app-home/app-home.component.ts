2fc4969a5ae450396c3d20de6774c465cc888e71
SabikAbtahee <sabik.abtahee@iqvia.com>
2025-12-03 08:17:36 +0600

    chore: remove about import

--- src/app/app-home/app-home.component.ts
+++ src/app/app-home/app-home.component.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { COMMON_IMPORTS, SHARED_COMPONENTS } from '@shared';
 
-import { AboutComponent } from '../about/about.component';
-
 @Component({
 	selector: 'app-home',
-	imports: [SHARED_COMPONENTS, COMMON_IMPORTS, AboutComponent],
+	imports: [SHARED_COMPONENTS, COMMON_IMPORTS],
 	templateUrl: './app-home.component.html',
 	styleUrl: './app-home.component.scss',
 	changeDetection: ChangeDetectionStrategy.OnPush
