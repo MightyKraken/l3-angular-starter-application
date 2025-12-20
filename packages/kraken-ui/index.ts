1e22db38bba6210629b585a9e3c7c6e7b3b38ef1
SabikAbtahee <sabik.abtahee@iqvia.com>
2025-12-20 20:55:23 +0600

    chore: Optimize imports

--- packages/kraken-ui/index.ts
+++ packages/kraken-ui/index.ts
@@ -1,4 +1,5 @@
 // @index(['./**/*.ts', '!./**/*.spec.ts'], f => `export * from '${f.path}';`)
-export * from './components/button/base-button/base-button.component';
-export * from './components/button/icon-button/icon-button.component';
-export * from './components/button/icon-text-button/icon-text-button.component';
+export * from './lib/components/button/base-button/base-button.component';
+export * from './lib/components/button/icon-button/icon-button.component';
+export * from './lib/components/button/icon-text-button/icon-text-button.component';
+
