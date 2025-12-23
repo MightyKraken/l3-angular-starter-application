eb59a0ee74edd171fb81f74cfd0cfdc0f0cd9f05
SabikAbtahee <sabik.abtahee@iqvia.com>
2025-12-23 20:39:50 +0600

    chore: remove packages

--- packages/kraken-ui/index.ts
+++
@@ -1,5 +0,0 @@
-// @index(['./**/*.ts', '!./**/*.spec.ts'], f => `export * from '${f.path}';`)
-export * from './lib/components/button/base-button/base-button.component';
-export * from './lib/components/button/icon-button/icon-button.component';
-export * from './lib/components/button/icon-text-button/icon-text-button.component';
-
